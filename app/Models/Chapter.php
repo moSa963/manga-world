30472b72138d511b9dd50ff00106af7557cbd082
moSa963 <mo.sa963@hotmail.com>
2024-01-27 21:17:40 +0300

    Add chapter model and migration

---
+++ app/Models/Chapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chapter extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "series_id",
+        "title",
+        "published",
+    ];
+
+    public function series() {
+        return $this->belongsTo(Series::class);
+    }
+
+}
