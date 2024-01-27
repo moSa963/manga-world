bd2141c402b57916679e548dc9f87979d5946efb
moSa963 <mo.sa963@hotmail.com>
2024-01-27 19:50:58 +0300

    Add SeriesStatus enum

---
+++ app/Enums/SeriesStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum SeriesStatus:string {
+    case ONGOING = "ongoing";
+    case STOPPED = "stopped";
+    case FINISHED = "finished";
+
+    public static function values() : array {
+        return array_column(self::cases(), "value");
+    }
+}
