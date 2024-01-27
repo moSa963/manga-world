6085d0529d41703f5d996a8fcc5ec7fa2816d463
moSa963 <mo.sa963@hotmail.com>
2024-01-27 21:19:06 +0300

    Add chapters method to Series model

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -24,4 +24,8 @@ class Series extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function chapters() {
+        return $this->hasMany(Chapter::class);
+    }
 }
