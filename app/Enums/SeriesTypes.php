a42c1bc70526eb41c06007b5d329231686c6cbc0
moSa963 <mo.sa963@hotmail.com>
2024-01-27 19:51:08 +0300

    Add SeriesTypes enum

---
+++ app/Enums/SeriesTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum SeriesTypes:string {
+    case MANGA = "manga";
+    case MANHWA = "manhwa";
+
+    public static function values() : array {
+        return array_column(self::cases(), "value");
+    }
+}
