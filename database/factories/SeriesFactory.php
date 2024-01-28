5c20b6390f7b378bb7dd88ad007291fcb2adb089
moSa963 <mo.sa963@hotmail.com>
2024-01-28 18:29:26 +0300

    Add SeriesFactory

---
+++ database/factories/SeriesFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\SeriesStatus;
+use App\Enums\SeriesTypes;
+use App\Models\Series;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Series>
+ */
+class SeriesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "user_id" => null,
+            "title" => $this->faker->title(),
+            "description" => $this->faker->paragraph(),
+            "painter" => $this->faker->name(),
+            "author" => $this->faker->name(),
+            "other_names" => $this->faker->name(),
+            "type" => SeriesTypes::MANGA,
+            "status" => SeriesStatus::ONGOING,
+            "release_year" => "2018",
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterMaking(function(Series $series) {
+            if ($series->user_id == null) {
+                $series->user_id = User::factory()->createOne()->id;
+            }
+        });
+    }
+}
