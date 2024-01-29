7c2925affa235fe654fe29bf74759aa212e6669b
moSa963 <mo.sa963@hotmail.com>
2024-01-29 21:50:56 +0300

    Rename Welcome.vue to IndexPage.vue

---
+++ resources/js/Pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps<{
+    canLogin?: boolean;
+    canRegister?: boolean;
+    laravelVersion: string;
+    phpVersion: string;
+}>();
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+</template>
+
+<style>
+
+</style>
