8ed049b6b5f997d878a4510d2bf3a3314e27b51f
moSa963 <mo.sa963@hotmail.com>
2024-02-03 22:32:06 +0300

    Add AppBar component

---
+++ resources/js/Layouts/AppLayout/AppBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts" >
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
+import { interpolate } from '@/utils/Interpolator';
+import { ref } from 'vue';
+
+const p = ref<ScrollType>({
+    value: 0,
+    ratio: 0,
+    innerHeight: 0,
+});
+
+const handleChange = (scroscroll: ScrollType) => {
+    p.value = scroscroll;
+}
+
+</script>
+
+
+
+<template>
+    <WindowScroll @change="handleChange">
+        <div class="sticky top-0 flex w-full h-32 ">
+            <div class="w-full h-12 flex justify-center items-center border-black border-b-[1px]"
+                :style="{ margin: `${interpolate(p.value, [0, 150], [30, 0])}px` }">
+                <ApplicationLogo class="h-48 fill-current text-gray-600"
+                    :style="{ transform: `scale(${interpolate(p.value, [0, 150], [1, 0.5])})`, }" />
+            </div>
+        </div>
+    </WindowScroll>
+</template>
