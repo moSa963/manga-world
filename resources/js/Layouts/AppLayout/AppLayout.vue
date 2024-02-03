7ea4a3e1176fd7be4383c08b8eb47d491000dee3
moSa963 <mo.sa963@hotmail.com>
2024-02-03 22:32:13 +0300

    Add AppLayout component

---
+++ resources/js/Layouts/AppLayout/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppBar from './AppBar.vue';
+</script>
+
+<template>
+    <div class="min-h-screen relative">
+        <AppBar />
+
+        <slot />
+    </div>
+</template>
