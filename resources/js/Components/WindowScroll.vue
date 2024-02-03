9edfee2a3c8548daf980e326ab0216acd582be98
moSa963 <mo.sa963@hotmail.com>
2024-02-03 22:27:56 +0300

    Add WindowScroll component

---
+++ resources/js/Components/WindowScroll.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts" >
+import { onUnmounted } from 'vue';
+import { onMounted } from 'vue';
+
+export interface ScrollType {
+    value: number,
+    innerHeight: number,
+    ratio: number
+}
+
+const emits = defineEmits<{
+    "change": [scroll: ScrollType],
+}>();
+
+const onScroll = () => {
+    emits("change", {
+        value: window.scrollY,
+        innerHeight: document.body.clientHeight,
+        ratio: window.scrollY / (document.body.clientHeight - window.innerHeight),
+    });
+}
+
+onMounted(() => {
+    window.addEventListener("scroll", onScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener("scroll", onScroll);
+});
+
+</script>
+
+
+<template>
+    <slot />
+</template>
