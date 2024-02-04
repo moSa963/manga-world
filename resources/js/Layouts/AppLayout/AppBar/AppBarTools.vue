62335c838647e716b6b405e161dd3556112e46b6
moSa963 <mo.sa963@hotmail.com>
2024-02-04 20:57:11 +0300

    Add AppBarTools

---
+++ resources/js/Layouts/AppLayout/AppBar/AppBarTools.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import AppBarItem from './AppBarItem.vue';
+import ViewListIcon from 'vue-material-design-icons/ViewList.vue';
+import HomeIcon from 'vue-material-design-icons/Home.vue';
+import InformationVariant from 'vue-material-design-icons/InformationVariant.vue';
+
+
+</script>
+
+<template>
+    <div class="h-full flex flex-1">
+        <AppBarItem first>
+            <template #icon>
+                <HomeIcon #icon />
+            </template>
+            <p>Home</p>
+        </AppBarItem>
+        <AppBarItem>
+            <template #icon>
+                <ViewListIcon #icon />
+            </template>
+            <p>List</p>
+        </AppBarItem>
+        <AppBarItem last>
+            <template #icon>
+                <InformationVariant #icon />
+            </template>
+            <p>About us</p>
+        </AppBarItem>
+    </div>
+</template>
