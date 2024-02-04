1578cf78ce267f47bc29522d2a1819fa3bf58d2d
moSa963 <mo.sa963@hotmail.com>
2024-02-04 20:58:24 +0300

    Move AppLayout to AppLayout folder

--- resources/js/Layouts/AppLayout/AppBar/AppBar.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBar.vue
@@ -3,6 +3,8 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
 import { interpolate } from '@/utils/Interpolator';
 import { ref } from 'vue';
+import AppBarTools from './AppBarTools.vue';
+
 
 const p = ref<ScrollType>({
     value: 0,
@@ -23,8 +25,15 @@ const handleChange = (scroscroll: ScrollType) => {
         <div class="sticky top-0 flex w-full h-32 ">
             <div class="w-full h-12 flex justify-center items-center border-black border-b-[1px]"
                 :style="{ margin: `${interpolate(p.value, [0, 150], [30, 0])}px` }">
-                <ApplicationLogo class="h-48 fill-current text-gray-600"
+
+                <AppBarTools />
+
+                <ApplicationLogo class="h-48 fill-current text-primary-500"
                     :style="{ transform: `scale(${interpolate(p.value, [0, 150], [1, 0.5])})`, }" />
+
+                <div class="h-full flex flex-1 justify-end">
+
+                </div>
             </div>
         </div>
     </WindowScroll>
--- resources/js/Layouts/AppLayout/AppLayout.vue
+++ resources/js/Layouts/AppLayout/AppLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppBar from './AppBar.vue';
+import AppBar from './AppBar/AppBar.vue';
 </script>
 
 <template>
