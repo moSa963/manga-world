32c558c98b8d77a70206f8b35ba9f314233a003e
moSa963 <mo.sa963@hotmail.com>
2024-02-09 22:20:11 +0300

    Add divider color

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,8 @@ export default {
                 'surface-800': 'rgb(var(--surface-800))',
                 'surface-900': 'rgb(var(--surface-900))',
                 'surface-950': 'rgb(var(--surface-950))',
+
+                'divider': 'rgb(var(--divider-primary) / var(--tw-bg-opacity))',
             },
         },
     },
