ae3e068355a52e41b711d59cd53c9d9020db7b06
moSa963 <mo.sa963@hotmail.com>
2024-02-10 18:01:00 +0300

    Add AppBarAuthItem

---
+++ resources/js/Layouts/AppLayout/AppBar/AppBarAuthItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <div
+        class="h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center select-none cursor-pointer bg-slate-500 border-[1px] border-inherit border-b-0 rounded-t overflow-hidden">
+        <p class="p-1 hidden sm:block">username</p>
+        <div class="aspect-square h-full">
+            <img class="w-full h-full object-cover" src="one.jpg" />
+        </div>
+    </div>
+</template>
