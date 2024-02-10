b792fa2c19685c1d74539e55efbb6996331dab5a
moSa963 <mo.sa963@hotmail.com>
2024-02-10 17:46:11 +0300

    Update AppBarItem style

--- resources/js/Layouts/AppLayout/AppBar/AppBarItem.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBarItem.vue
@@ -5,7 +5,7 @@ withDefaults(
         first: boolean,
         last: boolean,
         href: string | undefined,
-        simple: boolean
+        simple: boolean,
     }>(),
     {
         first: false,
@@ -18,8 +18,8 @@ withDefaults(
 </script>
 
 <template>
-    <a :href="href" :class="`h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center p-1 select-none cursor-pointer bg-slate-500 border-t-2 
-        ${first ? 'rounded-tl-md border-l-2' : ''} ${last ? 'rounded-tr-md border-r-2' : ''}`">
+    <a :href="href" :class="`h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center p-1 select-none cursor-pointer bg-slate-500 border-t-[1px] border-inherit
+                    ${first ? 'rounded-tl-md border-l-[1px]' : ''} ${last ? 'rounded-tr-md border-r-[1px]' : ''}`">
         <slot name="icon" />
         <slot v-if="!simple" />
     </a>
