6599fb5c3699a18b3552f1d84e19aaae6b96b013
moSa963 <mo.sa963@hotmail.com>
2024-02-10 17:50:54 +0300

    Add hidden class to text on small screens

--- resources/js/Layouts/AppLayout/AppBar/AppBarTools.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBarTools.vue
@@ -4,28 +4,31 @@ import ViewListIcon from 'vue-material-design-icons/ViewList.vue';
 import HomeIcon from 'vue-material-design-icons/Home.vue';
 import InformationVariant from 'vue-material-design-icons/InformationVariant.vue';
 
+defineProps<{
+    ratio: number,
+}>();
 
 </script>
 
 <template>
-    <div class="h-full flex flex-1">
+    <div class="h-full flex flex-1 border-inherit">
         <AppBarItem first>
             <template #icon>
                 <HomeIcon #icon />
             </template>
-            <p>Home</p>
+            <p class="text-primary hidden sm:block"> Home</p>
         </AppBarItem>
         <AppBarItem>
             <template #icon>
                 <ViewListIcon #icon />
             </template>
-            <p>List</p>
+            <p class="hidden sm:block">List</p>
         </AppBarItem>
         <AppBarItem last>
             <template #icon>
                 <InformationVariant #icon />
             </template>
-            <p>About us</p>
+            <p class="hidden sm:block">About us</p>
         </AppBarItem>
     </div>
 </template>
