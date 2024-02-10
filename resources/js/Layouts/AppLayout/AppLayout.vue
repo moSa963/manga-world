115557a9f31ffd89ded46e7ee623203ddcfb5dcc
moSa963 <mo.sa963@hotmail.com>
2024-02-10 20:35:26 +0300

    Add AppLayout theme and screenInfo

--- resources/js/Layouts/AppLayout/AppLayout.vue
+++ resources/js/Layouts/AppLayout/AppLayout.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import AppBar from './AppBar/AppBar.vue';
+import { onMounted } from 'vue';
+import { onUnmounted } from 'vue';
+import { provide } from 'vue';
+import { ScreenInfo } from '@/types';
+
+const theme = ref<"light" | "dark">("dark");
+
+const handleThemeModeChange = (mode: string) => {
+    theme.value = mode == "dark" ? "light" : "dark";
+}
+
+const screenInfo = ref<ScreenInfo>({ size: "sm" });
+const matchMedia = window.matchMedia('(min-width: 640px)');
+
+const onResize = () => {
+    screenInfo.value = {
+        size: matchMedia.matches ? "lg" : "sm",
+    };
+}
+
+onMounted(() => {
+    screenInfo.value = {
+        size: matchMedia.matches ? "lg" : "sm",
+    };
+    matchMedia.addEventListener("change", onResize);
+});
+
+
+onUnmounted(() => {
+    matchMedia.removeEventListener("change", onResize);
+});
+
+provide("screenInfo", screenInfo);
+
 </script>
 
 <template>
-    <div class="min-h-screen relative">
-        <AppBar />
+    <div :class="`${theme} min-h-screen relative divide-primary bg-surface-50 text-primary !border-primary`">
 
         <slot />
     </div>
