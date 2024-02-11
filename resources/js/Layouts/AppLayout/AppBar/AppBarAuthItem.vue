679b54836b344227722c8846b68fceae114513b3
moSa963 <mo.sa963@hotmail.com>
2024-02-11 17:43:33 +0300

    Add user to AppBarAuthItem

--- resources/js/Layouts/AppLayout/AppBar/AppBarAuthItem.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBarAuthItem.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import { Link, usePage } from '@inertiajs/vue3';
+
+
+const user = usePage().props.auth.user;
+
 </script>
 
 
 <template>
     <div
         class="h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center select-none cursor-pointer bg-slate-500 border-[1px] border-inherit border-b-0 rounded-t overflow-hidden">
-        <p class="p-1 hidden sm:block">username</p>
-        <div class="aspect-square h-full">
+        <p v-if="user != null" class="p-1 hidden sm:block">{{ user.username }}</p>
+        <div v-if="user != null" class="aspect-square h-full">
             <img class="w-full h-full object-cover" src="one.jpg" />
         </div>
+
+        <Link v-if="user == null" :href="route('login')" class="px-3">
+        Login
+        </Link>
     </div>
 </template>
