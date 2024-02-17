d3f65724e915d28d553a8e925ff3c403b3031cb6
moSa963 <mo.sa963@hotmail.com>
2024-02-17 20:44:39 +0300

    Add Chip component

---
+++ resources/js/Components/Chip.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const emits = defineEmits<{
+    click: [],
+}>();
+
+const handleClick = (payload: MouseEvent) => {
+    emits("click");
+}
+</script>
+
+<template>
+    <div class="px-1 h-fit border-primary border-[1px] rounded-lg cursor-pointer hover:bg-surface-600" @click="handleClick">
+        <slot />
+    </div>
+</template>
