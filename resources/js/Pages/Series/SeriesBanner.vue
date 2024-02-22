b4179e9df5cd09053c7ba5383c5475c2431e74f9
moSa963 <mo.sa963@hotmail.com>
2024-02-22 22:11:18 +0300

    Add SeriesBanner

---
+++ resources/js/Pages/Series/SeriesBanner.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
+import { interpolate } from '@/utils/Interpolator';
+import { ref } from 'vue';
+
+const p = ref<ScrollType>({
+    value: 0,
+    ratio: 0,
+    innerHeight: 0,
+});
+const handleChange = (scroscroll: ScrollType) => {
+    p.value = scroscroll;
+}
+</script>
+
+
+<template>
+    <WindowScroll @change="handleChange">
+        <div class="relative w-full bg-inherit flex rounded-2xl overflow-hidden"
+            :style="{ transform: `translateY(${interpolate(p.value, [55, 250], [0, 250 - 55])}px)` }">
+
+            <div class="h-full aspect-[2/3] overflow-hidden rounded-2xl rounded-e-none border-[1px] border-primary"
+                :style="{ width: `${interpolate(p.value, [55, 100], [288, 144])}px` }">
+                <img src="/one.jpg" class="rounded-2xl rounded-e-none border-[6px] border-surface-50" />
+            </div>
+
+            <div class="relative flex-1 overflow-hidden">
+                <div class="absolute inset-0 flex flex-col">
+                    <div class="bg-surface-100"
+                        :style="{ transform: `translateY(${interpolate(p.value, [100, 200], [0, 70])}%)`, }">
+                        <p class="font-extrabold font-serif text-5xl p-3">One Piece</p>
+                        <hr />
+                    </div>
+                    <div class="bg-surface-200 flex-1 overflow-hidden p-5"
+                        :style="{ opacity: `${interpolate(p.value, [100, 200], [1, 0])}`, }">
+                        <p>SUMMARY</p>
+                        <p>SUMMARY</p>
+                        <p>SUMMARY</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </WindowScroll>
+</template>
