434b24c5b369fcb82ee010be3033fd08994a4d76
moSa963 <mo.sa963@hotmail.com>
2024-02-22 22:12:06 +0300

    Add ShowSeriesPage

---
+++ resources/js/Pages/Series/ShowSeriesPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppLayout from '../../Layouts/AppLayout/AppLayout.vue';
+import SeriesBanner from './SeriesBanner.vue';
+
+</script>
+
+
+<template>
+    <AppLayout>
+        <SeriesBanner />
+    </AppLayout>
+</template>
