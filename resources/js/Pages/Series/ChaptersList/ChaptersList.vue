e3dc8af2fcbeb9020d7919bf120f797ad0bc299f
moSa963 <mo.sa963@hotmail.com>
2024-02-24 19:59:33 +0300

    Add ChaptersList

---
+++ resources/js/Pages/Series/ChaptersList/ChaptersList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ListItem from './ListItem.vue';
+
+</script>
+
+<template>
+    <div class="w-full border-[1px]">
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <div class="">
+
+        </div>
+        <ListItem />
+        <ListItem />
+        <ListItem />
+    </div>
+</template>
---
+++ resources/js/Pages/Series/ChaptersList/ListItem.vue
@@ -0,0 +1,9 @@
+
+
+<template>
+    <div class="bg-surface-100 max-w-full m-1 cursor-pointer hover:bg-surface-200">
+        <div class="w-full p-2">
+            <p>1 - Romance Dawn</p>
+        </div>
+    </div>
+</template>
