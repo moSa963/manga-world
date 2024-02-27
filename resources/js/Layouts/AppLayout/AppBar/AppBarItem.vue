20809ca3c2ae0e33d2683f2c40c607bf6e6a1824
moSa963 <mo.sa963@hotmail.com>
2024-02-27 21:27:11 +0300

    Fix AppBarItem link

--- resources/js/Layouts/AppLayout/AppBar/AppBarItem.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBarItem.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
 
 withDefaults(
     defineProps<{
@@ -18,9 +20,9 @@ withDefaults(
 </script>
 
 <template>
-    <a :href="href" :class="`h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center p-1 select-none cursor-pointer bg-slate-500 border-t-[1px] border-inherit
+    <Link :href="href || ''" :class="`h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center p-1 select-none cursor-pointer bg-slate-500 border-t-[1px] border-inherit
                     ${first ? 'rounded-tl-md border-l-[1px]' : ''} ${last ? 'rounded-tr-md border-r-[1px]' : ''}`">
-        <slot name="icon" />
-        <slot v-if="!simple" />
-    </a>
+    <slot name="icon" />
+    <slot v-if="!simple" />
+    </Link>
 </template>
