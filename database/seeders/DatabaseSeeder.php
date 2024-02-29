8b448be5b39ac8a8ab296b228840616008fad95e
moSa963 <mo.sa963@hotmail.com>
2024-02-29 21:21:16 +0300

    Add series database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Chapter;
+use App\Models\Page;
+use App\Models\Series;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create(["username" => "admin"]);
+
+        $series = Series::factory(10)->create();
+
+        foreach ($series as $s) {
+            for ($i = 0; $i < 5; ++$i) {
+                $chapter = Chapter::factory()->createOne(["series_id" => $s->id]);
+                for ($i = 0; $i < 5; ++$i) {
+                    Page::factory()->createOne(["chapter_id" => $chapter->id]);
+                }
+            }
+        }
     }
 }
