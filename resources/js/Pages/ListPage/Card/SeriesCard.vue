eb13362f3c41f266e4d315e58a97ab5eb450290a
moSa963 <mo.sa963@hotmail.com>
2024-03-02 18:56:02 +0300

    Add Link and image route src to SeriesCard

--- resources/js/Pages/ListPage/Card/SeriesCard.vue
+++ resources/js/Pages/ListPage/Card/SeriesCard.vue
@@ -4,6 +4,7 @@ import { Series } from '@/types';
 import CardTitle from './CardTitle.vue';
 import CardImg from './CardImg.vue';
 import Chip from '@/Components/Chip.vue';
+import { Link } from '@inertiajs/vue3';
 
 const hover = ref(false);
 
@@ -22,27 +23,27 @@ const handleMouseLeave = () => {
 </script>
 
 <template>
-    <div class="relative overflow-hidden flex justify-center items-center transition-transform rounded-2xl border-[1px] border-primary p-1 cursor-pointer select-none"
+    <Link :href="route('series.show', series.id)"
+        class="relative overflow-hidden flex justify-center items-center transition-transform rounded-2xl border-[1px] border-primary p-1 cursor-pointer select-none"
         @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
 
-        <div class="w-full h-[20%] flex gap-2 justify-center items-center bg-surface-50 shadow-inner">
-            <Chip @click="() => console.log('chapter 1107')">
-                <p>1107</p>
-            </Chip>
-            <Chip @click="() => console.log('chapter 1106')">
-                <p>1106</p>
-            </Chip>
-        </div>
-
-        <div
-            class="absolute inset-0 overflow-hidden rounded-2xl border-surface-50 border-[3px] grayscale-0 hover:grayscale-[50%] pointer-events-none">
-
-            <CardImg :src="series.image" :open="hover" />
-
-            <CardTitle :hover="hover">
-                <p class="font-bold">{{ series.title }}</p>
-            </CardTitle>
-        </div>
+    <div class="w-full h-[20%] flex gap-2 justify-center items-center bg-surface-50 shadow-inner">
+        <Chip @click="() => console.log('chapter 1107')">
+            <p>1107</p>
+        </Chip>
+        <Chip @click="() => console.log('chapter 1106')">
+            <p>1106</p>
+        </Chip>
     </div>
-</template>
 
+    <div
+        class="absolute inset-0 overflow-hidden rounded-2xl border-surface-50 border-[3px] grayscale-0 hover:grayscale-[50%] pointer-events-none">
+
+        <CardImg :src="route('api.series.poster', series.id)" :open="hover" />
+
+        <CardTitle :hover="hover">
+            <p class="font-bold">{{ series.title }}</p>
+        </CardTitle>
+    </div>
+    </Link>
+</template>
