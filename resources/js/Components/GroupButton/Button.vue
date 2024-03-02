6255e778ed9edb60f1b21dd33b9cb357b2fe7272
moSa963 <mo.sa963@hotmail.com>
2024-03-02 17:11:15 +0300

    Add Button and ButtonGroup

---
+++ resources/js/Components/GroupButton/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const emits = defineEmits<{
+    click: [],
+}>();
+
+const handleClick = () => {
+    emits("click");
+}
+</script>
+
+
+<template>
+    <div @click="handleClick"
+        class="relative flex justify-center items-center cursor-pointer p-1 py-2 rounded-lg hover:bg-primary-200/15 transition-colors border-primary-100/75 select-none overflow-hidden ">
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Components/GroupButton/ButtonGroup.vue
@@ -0,0 +1,6 @@
+<template>
+    <div
+        class="rounded-xl flex overflow-hidden [&>div]:rounded-none w-fit [&>div]:border-r-[1px] [&>div:last-child]:border-r-[0px]">
+        <slot />
+    </div>
+</template>
