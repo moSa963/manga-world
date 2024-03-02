0d193d90301715752b7cd6dc638b6ac79781d83e
moSa963 <mo.sa963@hotmail.com>
2024-03-02 18:57:40 +0300

    Add List to component to ListPage

---
+++ resources/js/Pages/ListPage/List.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Series } from '@/types';
+import Card from './Card/SeriesCard.vue';
+
+defineProps<{
+
+    series: Array<Series>,
+
+}>();
+
+
+</script>
+
+
+
+<template>
+    <div :class="`relative flex w-full max-w-4xl flex-wrap justify-center gap-2 sm:gap-5`">
+        <Card :series="card" :class="`w-full max-w-28 sm:max-w-52 shrink-0 aspect-square`"
+            v-for="(card, _) in series" />
+
+    </div>
+</template>
