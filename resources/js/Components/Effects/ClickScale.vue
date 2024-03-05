857fd26d164728fee30e719728e1b00d59ea08d2
moSa963 <mo.sa963@hotmail.com>
2024-03-05 21:16:03 +0300

    Add ClickScale

---
+++ resources/js/Components/Effects/ClickScale.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const click = ref(false);
+
+const emits = defineEmits<{
+    click: []
+}>();
+
+const handleDown = () => {
+    click.value = true;
+}
+
+const handleUp = () => {
+    click.value = false;
+}
+
+const handleClick = () => {
+    emits("click");
+}
+</script>
+
+<template>
+    <div @click="handleClick" @mousedown="handleDown" @mouseup="handleUp" @mouseleave="handleUp"
+        :style="{ transform: `scale(${click ? 0.99 : 1})` }">
+        <slot />
+    </div>
+</template>
