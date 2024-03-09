8cf809a3a292b352e3ff6162f1ba7bff693aa975
moSa963 <mo.sa963@hotmail.com>
2024-03-09 17:46:50 +0300

    Add ChapterChip

---
+++ resources/js/Pages/ListPage/Card/ChapterChip.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import Chip from '@/Components/Chip.vue';
+import { Chapter, Series } from '@/types';
+import { router } from '@inertiajs/vue3';
+
+const props = defineProps<{
+    series: Series,
+    chapter: Chapter,
+}>();
+
+const handleClick = () => {
+    router.get(route("chapter.show", {
+        series: props.series.id,
+        chapter: props.chapter.number,
+    }),
+    );
+}
+
+</script>
+
+<template>
+    <Chip @click.capture.prevent="handleClick" class="flex-1 overflow-hidden">
+        <p class="text-ellipsis overflow-hidden">{{ chapter.number }}</p>
+    </Chip>
+</template>
