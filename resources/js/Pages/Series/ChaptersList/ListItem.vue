c5f33fd94082afad35c24dbe5ab1b0db01fee3e7
moSa963 <mo.sa963@hotmail.com>
2024-03-10 13:28:08 +0300

    Add series and chapter props to chapter ListItem

--- resources/js/Pages/Series/ChaptersList/ListItem.vue
+++ resources/js/Pages/Series/ChaptersList/ListItem.vue
@@ -1,9 +1,23 @@
+<script setup lang="ts">
+import ClickScale from '@/Components/Effects/ClickScale.vue';
+import { Chapter, Series } from '@/types';
+import { Link } from '@inertiajs/vue3';
 
+
+defineProps<{
+    series: Series,
+    chapter: Chapter,
+}>();
+
+</script>
 
 <template>
-    <div class="bg-surface-100 max-w-full m-1 cursor-pointer hover:bg-surface-200">
-        <div class="w-full p-2">
-            <p>1 - Romance Dawn</p>
+    <ClickScale class="bg-surface-100 cursor-pointer hover:bg-surface-200 select-none">
+        <Link :href="route('chapter.show', { series: series.id, chapter: chapter.number, })">
+        <div class="w-full p-2 flex justify-between">
+            <p>{{ chapter.number }} - {{ chapter.title }}</p>
+            <p>{{ chapter.releaseDate }}</p>
         </div>
-    </div>
+        </Link>
+    </ClickScale>
 </template>
