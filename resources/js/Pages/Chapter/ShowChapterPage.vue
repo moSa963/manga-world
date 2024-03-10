a419e7547dc27e0b0e37f27adf10137a7aa6a0e4
moSa963 <mo.sa963@hotmail.com>
2024-03-10 13:24:16 +0300

    Add ShowChapterPage

---
+++ resources/js/Pages/Chapter/ShowChapterPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CircularProgress from '@/Components/CircularProgress.vue';
+import AppLayout from '@/Layouts/AppLayout/AppLayout.vue';
+import { Chapter, Series } from '@/types';
+import { ref } from 'vue';
+
+const props = defineProps<{
+    chapter: Chapter,
+    series: Series,
+}>();
+
+const data = ref<Array<string>>([]);
+
+</script>
+
+<template>
+    <AppLayout :title="chapter.series">
+        <img v-for="(item, index) in data" :key="index" :src="item" />
+        <CircularProgress class="size-24" v-if="data.length != chapter.pages.length" />
+    </AppLayout>
+</template>
