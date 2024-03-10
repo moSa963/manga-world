d7cecf2540a1c38b840a552cc27a0d400a4a1e03
moSa963 <mo.sa963@hotmail.com>
2024-03-10 09:27:19 +0300

    Add background color to CardImg

--- resources/js/Pages/ListPage/Card/CardImg.vue
+++ resources/js/Pages/ListPage/Card/CardImg.vue
@@ -6,12 +6,12 @@ defineProps<{
 </script>
 
 <template>
-    <div class="absolute inset-0 overflow-hidden transition-transform"
+    <div class="absolute inset-0 overflow-hidden transition-transform  bg-surface-100"
         :style="{ 'clip-path': 'inset(0 0 50% 0)', transform: `translateY(${open ? -10 : 0}%)` }">
         <img :src="src" class="absolute inset-0 object-cover" />
     </div>
 
-    <div class="absolute inset-0 overflow-hidden transition-transform"
+    <div class="absolute inset-0 overflow-hidden transition-transform  bg-surface-100"
         :style="{ 'clip-path': 'inset(50% 0 0 0)', transform: `translateY(${open ? 10 : 0}%)` }">
         <img :src="src" class="object-cover" />
     </div>
