07f57fc2b7367a5ebc9c7e4e4c4f14b6d21549de
moSa963 <mo.sa963@hotmail.com>
2024-03-11 21:09:11 +0300

    Add title to AppLayout and series to ChaptersList in ShowSeriesPage

--- resources/js/Pages/Series/ShowSeriesPage.vue
+++ resources/js/Pages/Series/ShowSeriesPage.vue
@@ -2,17 +2,23 @@
 import AppLayout from '../../Layouts/AppLayout/AppLayout.vue';
 import ChaptersList from './ChaptersList/ChaptersList.vue';
 import SeriesBanner from './SeriesBanner.vue';
+import { Series } from '@/types';
+
+defineProps<{
+    series: Series,
+}>();
+
 
 </script>
 
 
 <template>
-    <AppLayout>
-        <SeriesBanner />
+    <AppLayout :title="series.title">
+        <SeriesBanner :series="series" />
         <div class="w-full flex pb-96 pt-11">
 
             <div class="flex-[2] overflow-hidden relative ">
-                <ChaptersList />
+                <ChaptersList :series="series" />
             </div>
 
             <div class="flex-1">
