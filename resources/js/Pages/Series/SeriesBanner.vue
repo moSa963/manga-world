4547a9029e6157e379969f7f08e0ace94325a5bd
moSa963 <mo.sa963@hotmail.com>
2024-03-11 21:08:12 +0300

    Add series prop to SeriesBanner

--- resources/js/Pages/Series/SeriesBanner.vue
+++ resources/js/Pages/Series/SeriesBanner.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
+import { Series } from '@/types';
 import { interpolate } from '@/utils/Interpolator';
 import { ref } from 'vue';
+
+
+defineProps<{
+    series: Series,
+}>();
 
 const p = ref<ScrollType>({
     value: 0,
     ratio: 0,
     innerHeight: 0,
 });
-const handleChange = (scroscroll: ScrollType) => {
-    p.value = scroscroll;
+const handleChange = (scroll: ScrollType) => {
+    p.value = scroll;
 }
 </script>
 
@@ -17,27 +23,26 @@ const handleChange = (scroscroll: ScrollType) => {
 <template>
     <WindowScroll @change="handleChange">
         <div class="relative w-full bg-inherit flex rounded-2xl overflow-hidden" :style="{
-            transform: `translateY(${interpolate(p.value, [55, 200], [0, 200 - 55])}px)`,
-            marginBottom: `${interpolate(p.value, [55, 200], [0, 200 - 55])}px`
-        }">
+        transform: `translateY(${interpolate(p.value, [55, 200], [0, 200 - 55])}px)`,
+        marginBottom: `${interpolate(p.value, [55, 200], [0, 200 - 55])}px`
+    }">
 
             <div class="h-full aspect-[2/3] overflow-hidden rounded-2xl rounded-e-none border-[1px] border-primary"
                 :style="{ width: `${interpolate(p.value, [55, 100], [22, 10])}%` }">
-                <img src="/one.jpg" class="rounded-2xl rounded-e-none border-[6px] !border-surface-50" />
+                <img :src="route('api.series.poster', series.id)"
+                    class="rounded-2xl rounded-e-none border-[6px] !border-surface-50" />
             </div>
 
             <div class="relative flex-1 overflow-hidden">
                 <div class="absolute inset-0 flex flex-col">
                     <div class="bg-surface-100"
-                        :style="{ transform: `translateY(${interpolate(p.value, [100, 200], [0, 70])}%)`, }">
-                        <p class="font-extrabold font-serif text-5xl p-3">One Piece</p>
+                        :style="{ transform: `translateY(${interpolate(p.value, [100, 200], [0, 50])}%)`, }">
+                        <p class="font-extrabold font-serif text-5xl p-3">{{ series.title }}</p>
                         <hr />
                     </div>
                     <div class="bg-surface-200 flex-1 overflow-hidden p-5"
                         :style="{ opacity: `${interpolate(p.value, [100, 200], [1, 0])}`, }">
-                        <p>SUMMARY</p>
-                        <p>SUMMARY</p>
-                        <p>SUMMARY</p>
+                        <p>{{ series.description }}</p>
                     </div>
                 </div>
             </div>
