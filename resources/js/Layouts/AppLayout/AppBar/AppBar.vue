a9e190f288871154499a72057f3d020e8b429105
moSa963 <mo.sa963@hotmail.com>
2024-03-13 20:54:58 +0300

    Update AppBar margin

--- resources/js/Layouts/AppLayout/AppBar/AppBar.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBar.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts" >
+<script setup lang="ts">
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
 import { interpolate } from '@/utils/Interpolator';
@@ -27,11 +27,11 @@ const emits = defineEmits<{
     "themeChange": [mode: string],
 }>();
 
-const handleChange = (scroscroll: ScrollType) => {
-    p.value = scroscroll;
+const handleChange = (scroll: ScrollType) => {
+    p.value = scroll;
 
 }
-const handleChangasde = () => {
+const handleThemeChange = () => {
     emits("themeChange", props.themeMode);
 }
 
@@ -50,10 +50,10 @@ const screen = inject<ScreenInfo>("screenInfo");
         <div class="sticky top-0 flex w-full h-24 sm:h-32 z-50">
             <div :class="`w-full h-10 sm:h-14 flex justify-center items-center border-primary border-b-[1px] ${p.value > 150 ? 'shadow-sm' : ''}`"
                 :style="{
-                    margin: `${interpolate(p.value, [0, 150], [30, 0])}px`,
-                    borderColor: `rgb(var(--border-primary) / ${interpolate(p.value, [0, 150], [1, 0])}`,
-                    backgroundColor: `rgb(var(--surface-0) / ${interpolate(p.value, [0, 150], [0, 1])}`
-                }">
+        margin: `${interpolate(p.value, [0, 150], [screen?.size == 'sm' ? 15 : 30, 0])}px`,
+        borderColor: `rgb(var(--border-primary) / ${interpolate(p.value, [0, 150], [1, 0])}`,
+        backgroundColor: `rgb(var(--surface-0) / ${interpolate(p.value, [0, 150], [0, 1])}`
+    }">
 
                 <AppBarTools v-if="!searchOpen || (screen?.size !== 'sm')"
                     :ratio="interpolate(p.value, [0, 150], [1, 0])" />
@@ -65,7 +65,7 @@ const screen = inject<ScreenInfo>("screenInfo");
 
                 <div class="h-full flex flex-1 justify-end border-inherit">
                     <SearchBar :open="searchOpen" @change="handleSearchChange" />
-                    <ThemeButton v-if="!searchOpen" :mode="themeMode" @change="handleChangasde" />
+                    <ThemeButton v-if="!searchOpen" :mode="themeMode" @change="handleThemeChange" />
                     <AppBarAuthItem v-if="!searchOpen" />
                 </div>
             </div>
