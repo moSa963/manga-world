464f692ebd1cdefd5cb7b2d2a6f09dedef03a8cc
moSa963 <mo.sa963@hotmail.com>
2024-03-18 21:02:39 +0300

    Add loadData method to ShowChapterPage

--- resources/js/Pages/Chapter/ShowChapterPage.vue
+++ resources/js/Pages/Chapter/ShowChapterPage.vue
@@ -2,7 +2,7 @@
 import CircularProgress from '@/Components/CircularProgress.vue';
 import AppLayout from '@/Layouts/AppLayout/AppLayout.vue';
 import { Chapter, Series } from '@/types';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const props = defineProps<{
     chapter: Chapter,
@@ -11,11 +11,31 @@ const props = defineProps<{
 
 const data = ref<Array<string>>([]);
 
+
+const loadData = async () => {
+
+    for (var page of props.chapter.pages) {
+        const res = await fetch(route("page.show", {
+            series: props.series.id,
+            chapter: props.chapter.number,
+            page: page.number,
+        }));
+
+        const blob = await res.blob();
+
+        data.value.push(URL.createObjectURL(blob));
+    }
+
+}
+
+onMounted(loadData);
 </script>
 
 <template>
     <AppLayout :title="chapter.series">
-        <img v-for="(item, index) in data" :key="index" :src="item" />
-        <CircularProgress class="size-24" v-if="data.length != chapter.pages.length" />
+        <div class="w-full flex flex-col items-center">
+            <img class="max-w-4xl mb-3" v-for="(item, index) in data" :key="index" :src="item" />
+            <CircularProgress class="size-24" v-if="data.length != chapter.pages.length" />
+        </div>
     </AppLayout>
 </template>
