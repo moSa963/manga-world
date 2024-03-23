503a9848fbb22d1a80b3357a4a0ad30c44c8c0e3
moSa963 <mo.sa963@hotmail.com>
2024-03-23 12:02:14 +0300

    Add IconButton

---
+++ resources/js/Components/IconButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ClickScale from './Effects/ClickScale.vue';
+
+defineEmits<{
+    click: [],
+}>();
+
+</script>
+
+<template>
+    <ClickScale @click="$emit('click')"
+        class="bg-surface-100 rounded-full p-2 ml-3 hover:outline-[1px] outline-0 outline-divider/75 outline">
+        <slot />
+    </ClickScale>
+</template>
