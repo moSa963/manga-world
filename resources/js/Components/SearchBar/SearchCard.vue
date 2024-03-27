9a32c0f0920a9452f94249e0516955d77966197e
moSa963 <mo.sa963@hotmail.com>
2024-03-27 21:53:31 +0300

    Add SearchCard

---
+++ resources/js/Components/SearchBar/SearchCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SearchCardItem from './SearchCardItem.vue';
+
+
+</script>
+
+<template>
+    <div class="absolute right-0 left-0  top-full border-[1px] border-t-0 bg-surface-50 overflow-auto select-none">
+        <slot />
+    </div>
+</template>
