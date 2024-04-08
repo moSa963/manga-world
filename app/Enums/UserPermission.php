2a14a2e8ac247e4428b40cdc83b26b295bd8e386
moSa963 <mo.sa963@hotmail.com>
2024-04-08 22:44:00 +0300

    Add delete and update permissions

--- app/Enums/UserPermission.php
+++ app/Enums/UserPermission.php
@@ -5,6 +5,8 @@ namespace App\Enums;
 enum UserPermission: string
 {
     case CREATE = "create";
+    case DELETE = "delete";
+    case UPDATE = "update";
     case APPROVE = "approve";
 
     public static function values(): array
