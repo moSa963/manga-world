981aeb995774333dd57965574323f9e023a217f6
moSa963 <mo.sa963@hotmail.com>
2024-04-11 22:21:02 +0300

    Add border prop to Button

--- resources/js/Components/ButtonGroup/Button.vue
+++ resources/js/Components/ButtonGroup/Button.vue
@@ -4,6 +4,7 @@ import ClickRipple from '../Effects/ClickRipple.vue';
 
 defineProps<{
     disabled?: boolean | null,
+    border?: boolean
 }>();
 
 defineEmits<{
@@ -16,7 +17,7 @@ defineEmits<{
 <template>
     <ClickRipple @click="$emit('click')" class="rounded-xl" :disabled="disabled">
         <div
-            class="relative flex justify-center items-center cursor-pointer p-1 py-2 hover:bg-primary-400/10 transition-colors border-primary-100/75 select-none overflow-hidden">
+            :class="`relative flex justify-center items-center cursor-pointer p-1 py-2 hover:bg-primary-400/10 transition-colors border-primary-100/75 select-none overflow-hidden ${border ? 'rounded-xl border-[1px]' : ''}`">
 
             <slot />
         </div>
