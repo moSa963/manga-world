8a950c7e017585856cd5bcb7253b004004d1ea6e
moSa963 <mo.sa963@hotmail.com>
2024-04-11 21:11:12 +0300

    Add MultiTextInput component

---
+++ resources/js/Components/Input/MultiTextInput/MultiTextInput.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import InputLabel from '@/Components/InputLabel.vue';
+import InputChip from './InputChip.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { ref } from 'vue';
+import Button from '@/Components/ButtonGroup/Button.vue';
+
+const v = ref("");
+const model = defineModel<string[]>({ default: [] });
+
+defineProps<{
+    label: string,
+}>();
+
+const handleAdd = () => {
+    if (v.value.trim() == "") {
+        return;
+    }
+
+    model.value = [...model.value, v.value];
+    v.value = ""
+}
+
+const handleRemove = (index: number) => {
+    model.value = model.value.filter((_, i) => i != index);
+}
+</script>
+
+
+<template>
+    <div class="w-full">
+        <InputLabel :value="label" />
+        <div class="flex items-center gap-1">
+            <TextInput type="text" class="mt-1 block w-full" v-model="v" placeholder="Name..."
+                @enter-press="handleAdd" />
+
+            <Button class="h-full" border @click="handleAdd">
+                <p class="px-3"> Add</p>
+            </Button>
+        </div>
+
+        <div class="flex gap-1 flex-wrap m-3 p-3 border-[1px] border-t-0 border-divider/25">
+            <InputChip v-for="(text, i) in modelValue" :key="i" :text="text" @click="() => handleRemove(i)" />
+        </div>
+    </div>
+</template>
