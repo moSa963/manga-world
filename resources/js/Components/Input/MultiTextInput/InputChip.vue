7e07e4baa5eab50c746569e68b1146a9f35ad1a9
moSa963 <mo.sa963@hotmail.com>
2024-04-11 21:10:36 +0300

    Add InputChip component

---
+++ resources/js/Components/Input/MultiTextInput/InputChip.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Chip from '@/Components/Chip.vue';
+
+defineProps<{
+    text: string,
+}>();
+
+defineEmits<{
+    click: [string],
+}>();
+
+</script>
+
+
+<template>
+    <Chip @click="$emit('click', text)" class="root flex pl-0">
+        <p class="pl-1">{{ text }}</p>
+    </Chip>
+</template>
+
+
+<style scoped>
+.root::before {
+    content: 'x';
+    background-color: rgba(255, 0, 0, 0.5);
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 0px;
+    overflow: hidden;
+    transition: 100ms;
+}
+
+.root:hover::before {
+    width: 20px;
+}
+</style>
