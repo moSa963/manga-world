b73327f2d82583ad8a0abe06005c55682004fa41
moSa963 <mo.sa963@hotmail.com>
2024-04-11 22:17:16 +0300

    Add Select component

---
+++ resources/js/Components/Input/Select.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{
+    name?: string,
+    options: string[],
+}>();
+
+const model = defineModel<string>();
+
+</script>
+
+<template>
+    <select :name="name" class="bg-surface-100" v-model="model">
+        <option v-for="(o, i) in options" :key="i" :value="o" class="p-4">{{ o }}</option>
+    </select>
+</template>
