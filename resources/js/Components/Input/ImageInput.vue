1e1b4ade1374b9c05590cc6c2f1cf41ef792746c
moSa963 <mo.sa963@hotmail.com>
2024-04-12 22:32:43 +0300

    Add ImageInput component

---
+++ resources/js/Components/Input/ImageInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ImagePlusIcon from 'vue-material-design-icons/ImagePlus.vue';
+
+const emit = defineEmits<{
+    change: [file: string, url: string],
+}>();
+
+
+const handleClick = (e: MouseEvent) => {
+    ((e.currentTarget as HTMLElement).children[0] as HTMLElement).click();
+}
+
+const handleChange = (e: Event) => {
+    const file = (e.currentTarget as HTMLFormElement).files[0]
+
+    if (!file) return;
+
+    emit('change', file, URL.createObjectURL(file));
+}
+</script>
+
+<template>
+    <div @click.self="handleClick"
+        class="flex justify-center items-center bg-gray-900/75 cursor-pointer text-white select-none">
+        <input type="file" accept="Image/*" class="hidden" @change="handleChange" />
+        <ImagePlusIcon size="100px" class="pointer-events-none" />
+    </div>
+</template>
