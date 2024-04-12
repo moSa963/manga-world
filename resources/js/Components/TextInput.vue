6d0bac55d45330f28c98ced6e161555d7331175d
moSa963 <mo.sa963@hotmail.com>
2024-04-12 22:31:58 +0300

    Add label to TextInput

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import InputLabel from './InputLabel.vue';
 
 const model = defineModel<string>({ required: true });
 
 const input = ref<HTMLInputElement | null>(null);
+
+defineProps<{
+    label?: string,
+    placeholder?: string,
+    type?: string,
+}>();
 
 onMounted(() => {
     if (input.value?.hasAttribute('autofocus')) {
@@ -25,6 +32,10 @@ const handelKey = (e: KeyboardEvent) => {
 </script>
 
 <template>
-    <input class="border-divider focus:border-primary-900 focus:ring-primary-900 rounded-md shadow-sm bg-inherit"
-        v-model="model" ref="input" @keypress="handelKey" />
+    <div class="bg-inherit border-none">
+        <InputLabel v-if="label" :value="label" />
+        <input :placeholder="placeholder" :type="type"
+            class="w-full border-divider focus:border-primary-900 focus:ring-primary-900 rounded-md shadow-sm bg-inherit"
+            v-model="model" ref="input" @keypress="handelKey" />
+    </div>
 </template>
