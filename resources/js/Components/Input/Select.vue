c2df08da3ca39efbadab7381c9e3d7743e4985ca
moSa963 <mo.sa963@hotmail.com>
2024-04-12 22:32:57 +0300

    Add label to Select

--- resources/js/Components/Input/Select.vue
+++ resources/js/Components/Input/Select.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import InputLabel from '../InputLabel.vue';
+
 
 defineProps<{
     name?: string,
     options: string[],
+    label?: string,
 }>();
 
 const model = defineModel<string>();
@@ -10,7 +13,10 @@ const model = defineModel<string>();
 </script>
 
 <template>
-    <select :name="name" class="bg-surface-100" v-model="model">
-        <option v-for="(o, i) in options" :key="i" :value="o" class="p-4">{{ o }}</option>
-    </select>
+    <div class="flex-grow flex items-center gap-3">
+        <InputLabel v-if="label" :value="label" />
+        <select :name="name" class="bg-surface-100" v-model="model">
+            <option v-for="(o, i) in options" :key="i" :value="o" class="p-4">{{ o }}</option>
+        </select>
+    </div>
 </template>
