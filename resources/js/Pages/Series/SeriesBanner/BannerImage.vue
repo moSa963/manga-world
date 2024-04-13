60b85cc54924eaec4839feeda8115a4e5ca6f932
moSa963 <mo.sa963@hotmail.com>
2024-04-13 20:37:04 +0300

    Refactor SeriesBanner for better performance

--- resources/js/Pages/Series/SeriesBanner/BannerImage.vue
+++ resources/js/Pages/Series/SeriesBanner/BannerImage.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
-import { ScrollType } from '@/Components/WindowScroll.vue';
-import { ScreenInfo } from '@/types';
-import { interpolate } from '@/utils/Interpolator';
-import { inject } from 'vue';
+
 
 defineProps<{
     src?: string,
-    scroll?: ScrollType
 }>();
-
-const screen = inject<ScreenInfo>("screenInfo");
 
 </script>
 
 
 <template>
-    <div class="aspect-[2/3] overflow-hidden rounded-xl sm:rounded-2xl rounded-e-none sm:rounded-e-none border-[1px] border-primary"
-        :style="{ width: `${interpolate(scroll?.value, [55, 100], screen?.size == 'sm' ? [40, 20] : [22, 10])}%` }">
+    <div
+        class="h-full aspect-[2/3] overflow-hidden rounded-xl sm:rounded-2xl rounded-e-none sm:rounded-e-none border-[1px] border-primary">
         <img :src="src"
             class="rounded-xl sm:rounded-2xl rounded-e-none sm:rounded-e-none border-[2px] md:border-[6px] !border-surface-50" />
     </div>
--- resources/js/Pages/Series/SeriesBanner/SeriesBanner.vue
+++ resources/js/Pages/Series/SeriesBanner/SeriesBanner.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
-import { Series } from '@/types';
+import { ScreenInfo, Series } from '@/types';
 import { interpolate } from '@/utils/Interpolator';
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 import BannerImage from './BannerImage.vue';
 import BannerInfo from './BannerInfo.vue';
 
@@ -19,20 +19,22 @@ const scroll = ref<ScrollType>({
 const handleChange = (v: ScrollType) => {
     scroll.value = v;
 }
+
+const screen = inject<ScreenInfo>("screenInfo");
+
 </script>
 
 
 <template>
     <WindowScroll @change="handleChange">
         <div class="relative w-full bg-inherit flex rounded-xl sm:rounded-2xl overflow-hidden" :style="{
-        transform: `translateY(${interpolate(scroll.value, [50, 200], [0, 200 - 50])}px)`,
-        marginBottom: `${interpolate(scroll.value, [55, 200], [0, 200 - 55])}px`
-    }">
-
+            height: `${interpolate(scroll.value, [50, 200], screen?.size == 'sm' ? [200, 100] : [384, 150])}px`,
+            transform: `translateY(${interpolate(scroll.value, [50, 200], [0, 200 - 50])}px)`, marginBottom:
+                `${interpolate(scroll.value, [55, 200], [0, 200 - 55])}px`
+        }">
             <BannerImage :src="route('api.series.poster', series.id)" :scroll="scroll" />
 
             <BannerInfo :series="series" :scroll="scroll" />
-
         </div>
     </WindowScroll>
 </template>
