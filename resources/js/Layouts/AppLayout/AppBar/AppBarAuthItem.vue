f6d1414fb02f8e447d33428fa4daf2621947d6eb
moSa963 <mo.sa963@hotmail.com>
2024-04-15 21:21:09 +0300

    Add user image route to AppBarAuthItem

--- resources/js/Layouts/AppLayout/AppBar/AppBarAuthItem.vue
+++ resources/js/Layouts/AppLayout/AppBar/AppBarAuthItem.vue
@@ -12,7 +12,7 @@ const user = usePage().props.auth.user;
         class="h-full hover:-translate-y-1 bg-opacity-5 flex justify-center items-center select-none cursor-pointer bg-slate-500 border-[1px] border-inherit border-b-0 rounded-t overflow-hidden">
         <p v-if="user != null" class="p-1 hidden sm:block">{{ user.username }}</p>
         <div v-if="user != null" class="aspect-square h-full">
-            <img class="w-full h-full object-cover" src="one.jpg" />
+            <img class="w-full h-full object-cover" :src="route('api.users.image.show', { user: user.username })" />
         </div>
 
         <Link v-if="user == null" :href="route('login')" class="px-3">
