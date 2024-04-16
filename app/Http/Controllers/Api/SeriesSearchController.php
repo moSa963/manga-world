f6c7b0e191da51d700c2a2a2d543222a6a2181df
moSa963 <mo.sa963@hotmail.com>
2024-04-16 21:24:15 +0300

    Fix SeriesSearchControllers index method returns hidden data

--- app/Http/Controllers/Api/SeriesSearchController.php
+++ app/Http/Controllers/Api/SeriesSearchController.php
@@ -11,12 +11,12 @@ class SeriesSearchController extends Controller
 {
     public function index(Request $request, string $key)
     {
-
-        //TODO
         $data = Series::for($request->user())
-            ->where("title", 'LIKE', "%$key%")
-            ->orWhere("other_names", 'LIKE', "%$key%")
-            ->orWhere('author', 'LIKE', "%$key%")
+            ->where(function ($q) use ($key) {
+                $q->where("title", 'LIKE', "%$key%")
+                    ->orWhere("other_names", 'LIKE', "%$key%")
+                    ->orWhere('author', 'LIKE', "%$key%");
+            })
             ->simplePaginate(5);
 
         return SeriesResource::collection($data);
