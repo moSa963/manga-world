bc39b0746146ade596837bfaa9d3e9a20effa1fe
moSa963 <mo.sa963@hotmail.com>
2024-04-20 16:21:33 +0300

    Add ScrollableBox component

---
+++ resources/js/Components/ScrollableBox.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ScrollType } from '@/Components/WindowScroll.vue';
+
+
+const emits = defineEmits<{
+    "scroll": [scroll: ScrollType],
+}>();
+
+const onScroll = (e: Event) => {
+    const t = (e.target as HTMLElement);
+    emits("scroll", {
+        value: t.scrollTop,
+        innerHeight: window.innerHeight,
+        ratio: t.scrollTop / (t.clientHeight - window.innerHeight),
+    });
+}
+
+</script>
+
+<template>
+    <div @scroll="onScroll">
+        <slot />
+    </div>
+</template>
