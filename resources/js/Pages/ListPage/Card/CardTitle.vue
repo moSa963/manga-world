bf04a8c5854db704726a34c601f1f2a062b6ced7
moSa963 <mo.sa963@hotmail.com>
2024-04-25 21:39:52 +0300

    Update CardTitle background color

--- resources/js/Pages/ListPage/Card/CardTitle.vue
+++ resources/js/Pages/ListPage/Card/CardTitle.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
     <div
-        :class="`absolute transition-all max-h-[30%] ${hover ? 'bottom-0 rounded-none left-0 right-0' : 'bottom-5 rounded-xl left-1 right-1'} overflow-hidden text-center bg-surface-50 py-1 bg-opacity-20`">
+        :class="`absolute transition-all max-h-[30%] ${hover ? 'bottom-0 rounded-none left-0 right-0' : 'bottom-5 rounded-xl left-1 right-1'} overflow-hidden text-center bg-surface-50/95 py-1`">
         <slot />
     </div>
 </template>
