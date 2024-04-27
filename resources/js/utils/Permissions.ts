7d3b2db911a73a2e31deb3a1f4d834e01c22b2de
moSa963 <mo.sa963@hotmail.com>
2024-04-27 19:20:17 +0300

    Fix hasPermission function return wrong results

--- resources/js/utils/Permissions.ts
+++ resources/js/utils/Permissions.ts
@@ -3,5 +3,6 @@ import { Permission, User } from "@/types";
 
 
 export const hasPermission = (user: User | undefined | null, permission: Permission) => {
-    return user && (user.admin || (user.permissions?.findIndex(e => e == permission) || -1) != -1);
+    const res = user?.permissions?.findIndex(e => e == permission);
+    return user && (user.admin || (res != null && res > -1));
 }
