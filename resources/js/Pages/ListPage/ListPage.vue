921ba355cd730d2a04ab12e2120998dbbb21a88f
moSa963 <mo.sa963@hotmail.com>
2024-04-30 21:41:34 +0300

    Add the ability to filter the list of series according to publication status

--- resources/js/Pages/ListPage/ListPage.vue
+++ resources/js/Pages/ListPage/ListPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AppLayout from '@/Layouts/AppLayout/AppLayout.vue';
-import { Response, Series } from '@/types';
-import { ref } from 'vue';
+import { Response, Series, User } from '@/types';
+import { ref, watch } from 'vue';
 import { onMounted } from 'vue';
 import List from './List.vue';
 import CircularProgress from '@/Components/CircularProgress.vue';
@@ -10,34 +10,49 @@ import WindowScroll, { ScrollType } from '@/Components/WindowScroll.vue';
 import Button from '@/Components/ButtonGroup/Button.vue';
 import { Link } from '@inertiajs/vue3';
 import ToggleButton from '@/Components/Input/ToggleButton.vue';
+import { hasPermission } from '@/utils/Permissions';
 
 const data = ref<Response<Series> | null>(null);
 const progress = ref<boolean>(false);
 const circularProgress = ref<HTMLDivElement>();
+const publicationStatus = ref<string>();
 
 defineProps<{
     canCreateSeries?: boolean,
+    auth: {
+        user: User
+    }
+
 }>();
 
-const getData = async () => {
-    const url = data.value ? data.value.links.next : route("api.series.list");
-    await fetchList(data, url, progress);
-}
 
 onMounted(async () => {
     await getData();
 });
+
+watch(publicationStatus, async (newValue, oldValue) => {
+    if (newValue != oldValue) {
+        await getData(true);
+    }
+});
+
+const getData = async (reset?: boolean) => {
+    const url = (data.value && !reset) ? data.value.links.next : route("api.series.list", { filter: publicationStatus.value });
+    await fetchList(data, url, progress, reset);
+}
 
 const handleChange = (scroll: ScrollType) => {
     if ((circularProgress.value?.offsetTop || 0) - scroll.value < scroll.innerHeight) {
         getData();
     }
 }
+
 </script>
 
 <template>
     <AppLayout title="List">
         <WindowScroll @change="handleChange">
+
             <div class="w-full flex flex-col-reverse md:flex-row">
                 <div class="flex-1 items-center ">
                     <List v-if="data" :series="data.data" />
@@ -52,6 +67,9 @@ const handleChange = (scroll: ScrollType) => {
                         <p>Add new series</p>
                     </Button>
                     </Link>
+                    <div v-if="hasPermission(auth.user, 'approve')" class="w-full flex flex-col items-center mt-2">
+                        <ToggleButton allow-empty :values="['published', 'unpublished']" v-model="publicationStatus" />
+                    </div>
                 </div>
             </div>
         </WindowScroll>
