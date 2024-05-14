127098919be6a31da31d7496058bd0228315cb5d
moSa963 <mo.sa963@hotmail.com>
2024-05-14 20:43:05 +0300

    Fix SeriesSearchTest

--- tests/Feature/Api/SeriesSearchTest.php
+++ tests/Feature/Api/SeriesSearchTest.php
@@ -30,7 +30,7 @@ test('user can search for series', function () {
         "author" => "onwePiece",
     ]);
 
-    $response = get(route("api.series.list", ["key" => $key]));
+    $response = get(route("api.series.list", ["key" => $key, "sort" => 'old']));
 
     $response->assertStatus(200);
     $response->assertJsonCount(3, "data");
