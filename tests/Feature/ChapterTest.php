6455595dee3379a3598d6ef8175ff87106e3f33d
moSa963 <mo.sa963@hotmail.com>
2024-05-19 20:46:09 +0300

    Add CreateChapterPage tests

--- tests/Feature/ChapterTest.php
+++ tests/Feature/ChapterTest.php
@@ -1,13 +1,44 @@
 <?php
 
+use App\Enums\UserPermission;
 use App\Models\Chapter;
+use App\Models\Series;
+use App\Models\User;
 
 test('chapter page can be rendered', function () {
     $chapter =  Chapter::factory()->createOne();
 
-    $response = $this->get("/series/{$chapter->series->id}/chapter/{$chapter->number}");
-
-    $chapter->series->delete();
+    $response = $this->get(route('chapter.show', ["series" => $chapter->series->id, "chapter" => $chapter->number]));
 
     $response->assertStatus(200);
+});
+
+test('create chapter page can be rendered', function () {
+    $series =  Series::factory()->createOne();
+
+    $user = User::factory()->createOne();
+
+    $user->addPermission(UserPermission::CREATE);
+
+    $response = $this->actingAs($user)->get(route('chapter.create', ["series" => $series->id]));
+
+    $response->assertStatus(200);
+});
+
+test('create chapter page cannot be rendered for unauthorized users', function () {
+    $series =  Series::factory()->createOne();
+
+    $user = User::factory()->createOne();
+
+    $response = $this->actingAs($user)->get(route('chapter.create', ["series" => $series->id]));
+
+    $response->assertForbidden();
+});
+
+afterEach(function () {
+    $series = Series::all();
+
+    foreach ($series as $s) {
+        $s->delete();
+    }
 });
