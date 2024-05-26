e5363f09c7310b980d145d9a571736a94599bf0e
moSa963 <mo.sa963@hotmail.com>
2024-05-26 20:00:46 +0300

    Update Buttons border color

--- resources/js/Components/ButtonGroup/Button.vue
+++ resources/js/Components/ButtonGroup/Button.vue
@@ -18,7 +18,7 @@ defineEmits<{
 <template>
     <ClickRipple @click="(e) => $emit('click', e)" class="rounded-xl" :disabled="disabled">
         <div
-            :class="`relative flex justify-center items-center cursor-pointer p-1 py-2 hover:bg-primary-400/10 transition-colors border-primary-100/75 select-none overflow-hidden ${border ? 'rounded-xl border-[1px]' : ''}`">
+            :class="`relative flex justify-center items-center cursor-pointer p-1 py-2 hover:bg-primary-400/10 transition-colors border-surface-900 select-none overflow-hidden ${border ? 'rounded-xl border-[1px]' : ''}`">
             <p v-if="value" class="font-bold text-xl">{{ value }}</p>
             <slot v-else />
         </div>
