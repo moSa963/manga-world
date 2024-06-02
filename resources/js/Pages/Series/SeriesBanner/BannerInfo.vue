0023accbf582f0be094dd039b87b2f47d6fe9c10
moSa963 <mo.sa963@hotmail.com>
2024-06-02 11:51:13 +0300

    Add genres to BannerInfo

--- resources/js/Pages/Series/SeriesBanner/BannerInfo.vue
+++ resources/js/Pages/Series/SeriesBanner/BannerInfo.vue
@@ -20,9 +20,9 @@ defineProps<{
             <p class="font-extrabold font-serif text-lg sm:text-2xl md:text-4xl p-1 md:p-3">{{ series.title }}</p>
             <hr />
         </div>
-        <div class="bg-surface-200 flex-1 overflow-hidden p-2 md:p-5 flex flex-col"
+        <div class="bg-surface-200 flex-1 overflow-hidden p-2 md:p-5 md:pb-2 flex flex-col"
             :style="{ opacity: `${interpolate(scroll?.value, [100, 200], [1, 0])}`, }">
-            <div class="w-full flex mb-3 gap-3">
+            <div class="w-full flex mb-3 gap-3 flex-wrap">
                 <Chip :class="`border-0 ${series.status == 'stopped' ? 'bg-red-700/50' : 'bg-primary-500/25'}`">
                     {{ series.status }}
                 </Chip>
@@ -31,8 +31,14 @@ defineProps<{
                 <div class="flex-grow"></div>
                 <p class="text-primary-100 font-extrabold">{{ getYear(series.releaseDate) }}</p>
             </div>
-            <p class="text-sm sm:text-xl flex-1">{{ series.description }}</p>
+            <p class="text-sm sm:text-xl flex-1 overflow-hidden">{{ series.description }}</p>
             <slot />
+            <div class="w-full flex py-1 gap-2">
+                <p v-if="series.genres.length > 0" v-for="v in series.genres.split(',')"
+                    class="px-1 rounded-md bg-primary-300/10">
+                    {{ v }}
+                </p>
+            </div>
         </div>
     </div>
 </template>
